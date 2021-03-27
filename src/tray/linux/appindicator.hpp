02b8d8c93aeb339e5fa06f669a7717b3ae03630c
Curve <fynnbwdt@gmail.com>
2021-03-27 18:03:28 +0100

    feat: add appindicator implementation

---
+++ src/tray/linux/appindicator.hpp
@@ -0,0 +1,34 @@
+#include "../tray.hpp"
+#include <cassert>
+#include <libappindicator/app-indicator.h>
+
+namespace Soundux
+{
+    class Tray;
+    class TrayPreInitializer
+    {
+      protected:
+        TrayPreInitializer(const std::string &, const std::string &, Tray &);
+    };
+
+    class Tray : TrayPreInitializer, public BaseTray
+    {
+        friend class TrayPreInitializer;
+
+        AppIndicator *appIndicator;
+        static void callback(GtkWidget *, gpointer);
+        static GtkMenuShell *constructMenu(const std::vector<std::shared_ptr<TrayItem>> &items);
+
+      public:
+        template <typename... T>
+        Tray(std::string name, std::string icon, const T &...items)
+            : TrayPreInitializer(name, icon, *this), BaseTray(std::move(name), std::move(icon), items...)
+        {
+            update();
+        }
+
+        bool run() override;
+        void exit() override;
+        void update() override;
+    };
+} // namespace Soundux
