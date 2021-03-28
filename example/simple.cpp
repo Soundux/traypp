a6ee576b5a5ee5d99b4f82707db1cd689eecd3d2
Curve <fynnbwdt@gmail.com>
2021-03-28 13:04:23 +0200

    feat: add example

---
+++ example/simple.cpp
@@ -0,0 +1,23 @@
+#include "tray/linux/appindicator.hpp"
+#include <iostream>
+
+int main()
+{
+    Soundux::Tray tray(
+        "TestTray", "icon.png", Soundux::TrayButton("Test", [&] { tray.exit(); }),
+        Soundux::TrayCheck("Test Check", false, [](bool state) { std::cout << "Checked: " << state << std::endl; }),
+        Soundux::TrayCheck("Test Check 2", true, [](bool state) { std::cout << "Checked 2: " << state << std::endl; }),
+        Soundux::TraySubmenu("Submenu", Soundux::TrayButton("Some button", []() {
+                                 std::cout << "Submenu Button 1 pressed!" << std::endl;
+                             })));
+
+    auto *submenu = dynamic_cast<Soundux::TraySubmenu *>(tray.getChildren().back().get());
+    submenu->addItem(
+        Soundux::TrayButton("Another button", []() { std::cout << "Submenu Button 2 pressed!" << std::endl; }));
+
+    while (tray.run())
+    {
+    }
+
+    return 0;
+}
