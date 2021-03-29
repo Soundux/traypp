2a767b8f592ced65bdbeafc7fb8b9eb1321f0fc0
Curve <fynnbwdt@gmail.com>
2021-03-29 16:50:55 +0200

    chore: update example

--- example/simple/main.cpp
+++ example/simple/main.cpp
@@ -7,10 +7,7 @@ int main()
     Soundux::Tray tray(
         "TestTray", "icon.png",
         Soundux::TrayCheck("Test Check", false, [](bool state) { std::cout << "Checked: " << state << std::endl; }),
-        Soundux::TrayCheck("Test Check 2", true, [](bool state) { std::cout << "Checked 2: " << state << std::endl; }),
-        Soundux::TraySubmenu("Submenu", Soundux::TrayButton("Some button", []() {
-                                 std::cout << "Submenu Button 1 pressed!" << std::endl;
-                             })));
+        Soundux::TrayCheck("Test Check 2", true, [](bool state) { std::cout << "Checked 2: " << state << std::endl; }));
 #elif defined(_WIN32)
     Soundux::Tray tray(
         "TestTray", 0 /*ICON_RESOURCE*/,
@@ -21,9 +18,11 @@ int main()
                              })));
 #endif
 
-    auto *submenu = dynamic_cast<Soundux::TraySubmenu *>(tray.getChildren().back().get());
-    submenu->addItem(
-        Soundux::TrayButton("Another button", []() { std::cout << "Submenu Button 2 pressed!" << std::endl; }));
+    tray.addItem(Soundux::TraySubmenu(
+                     "Submenu", Soundux::TrayButton("Some button",
+                                                    []() { std::cout << "Submenu Button 1 pressed!" << std::endl; })))
+        ->addItem(
+            Soundux::TrayButton("Another button", []() { std::cout << "Submenu Button 2 pressed!" << std::endl; }));
 
     tray.addItem(Soundux::TrayButton("Exit", [&] { tray.exit(); }));
 
