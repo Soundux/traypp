ce4f14e7143719da28780d5bbd912084408df303
Curve <fynnbwdt@gmail.com>
2021-04-18 18:20:47 +0200

    feat: update example

--- example/simple/main.cpp
+++ example/simple/main.cpp
@@ -5,15 +5,13 @@ int main()
 {
     Tray::Tray tray("test", "icon.ico");
 
-    tray.addEntry(Tray::Button("Test"));
+    tray.addEntry(Tray::Button("Exit", [&] { tray.exit(); }));
     tray.addEntry(Tray::Button("Test"))->setDisabled(true);
     tray.addEntry(Tray::Seperator());
     tray.addEntry(Tray::Label("Test Label"));
     tray.addEntry(Tray::Toggle("Test Toggle", false, [](bool state) { printf("State: %i\n", state); }));
 
-    while (tray.run(true))
-    {
-    }
+    tray.run();
 
     return 0;
 }
